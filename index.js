195acd51c1a223e24582c0be416872724d8da67d
naveedkapoorkhan <naveedkapoorkhan@gmail.com>
2024-08-29 22:51:26 -0700

    this key world

--- index.js
+++ index.js
@@ -32,4 +32,19 @@ function App(app){
    
 }
 //App(2);
+//this key word in javascript
 
+const person1 = {
+  name: "Chris",
+  introduceSelf() {
+    console.log(`Hi! I'm ${this.name}.`);
+  },
+};
+
+const person2 = {
+  name: "Deepti",
+  introduceSelf() {
+    console.log(`Hi! I'm ${this.name}.`);
+  },
+};
+//person1.introduceSelf();
